6ce2084e17d68363b882dbf2f7a47f23c2c0ef7e
Dckap Interview <test@example.com>
2020-04-13 18:17:06 +0530

    Still working on login

--- src/main/components/Signup.jsx
+++ src/main/components/Signup.jsx
@@ -14,9 +14,13 @@ class SignUp extends React.Component {
     handleSubmit(e){
         e.preventDefault();
         let data = {
-            "name": this.name.value
+            "Firstname": this.FirstName.value,
+            "LastName": this.LastName.value,
+            "Email": this.Email.value,
+            "Mobile": this.Mobile.value,
+            "Username": this.Username.value
         }
-        Axios.post('http://127.0.0.1:8000/authenticate/signup/', data)
+        Axios.post('http://127.0.0.1:8000/authenticate/signup/', data, {"params": {"Username": this.Username.value}})
         .then(res =>{
             if(res.status === 200){
                 this.props.history.push('/authenticate')
@@ -36,16 +40,32 @@ class SignUp extends React.Component {
 
     render() {
         return (
-            <div className="register">
+            <div>
                 <ErrorBoundary error={this.state.error} />
                 <form onSubmit={(e) => this.handleSubmit(e)}>
                     <label>
-                    Name:
-                    <input type="text" id="name" className="form-control" ref={(input) => this.name = input} required></input>
+                    First Name:
+                    <input type="text" id="firstname" ref={(input) => this.FirstName = input} required></input>
+                    </label>
+                    <label>
+                    Last Name:
+                    <input type="text" id="lastname" ref={(input) => this.LastName = input} required></input>
+                    </label>
+                    <label>
+                    Email:
+                    <input type="text" id="email" ref={(input) => this.Email = input} required></input>
+                    </label>
+                    <label>
+                    Mobile:
+                    <input type="text" id="mobile" ref={(input) => this.Mobile = input} required></input>
+                    </label>
+                    <label>
+                    Username:
+                    <input type="text" id="username" ref={(input) => this.Username = input} required></input>
                     </label>
                     <input type="submit" value="Submit"/>
                 </form>
-        </div>
+            </div>
         )
     }
 }
